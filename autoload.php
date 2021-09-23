ba2a754cb0d4596c49bce121708fae4cd2452509
sadman <ahsan.susmoy@gmail.com>
2021-09-23 18:55:34 +0600

    Basic Files for CRUD management

---
+++ autoload.php
@@ -0,0 +1,7 @@
+<?php
+    include_once('app/auth.php');
+    include_once('app/functions.php');
+    include_once('app/db.php');
+    include_once('app/session.php');
+    include_once('app/cookie.php');
+?>
